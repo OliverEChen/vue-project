52c3663f4d3e967991a3179a52428388e15807e2
long <1663862286@qq.com>
2020-09-12 19:50:02 +0800

    搭好路由组件

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,22 @@
 const path = require('path')
+const px2rem = require('postcss-px2rem')
 
 module.exports = {
+  // runtimeCompiler: true,
+  // lintOnSave: false, // 关闭EsLint的规则
+  css: { // 添加postcss配置
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          // 配置postcss-px2rem
+          px2rem({
+            remUnit: 37.5   // 设计稿等分后的rem值   375/10
+          })
+        ]
+      }
+    }
+  },
+
   configureWebpack: {
     resolve: {
       extensions: ['.js', '.vue', '.json'], // 可以省略的后缀名
